32e44cd848b8245c258a3b44ddc0365546c3e9e0
jm1412 <112815299+jm1412@users.noreply.github.com>
2023-06-26 19:00:25 +0800

    Productivity App start

---
+++ app_calendar/static/app_calendar/app_calendar.js
@@ -0,0 +1,24 @@
+// Bindings
+const query = document.querySelector.bind(document);
+const queryAll = document.querySelectorAll.bind(document);
+const byId = document.getElementById.bind(document);
+const byClass = document.getElementsByClassName.bind(document);
+const byTag = document.getElementsByTagName.bind(document);
+
+function showRegister() {
+    console.log("function: showRegister")
+    byId("login-form").style.display = "none";
+    byId("register-form").style.display = "block";
+    return false;
+}
+
+function showLogin() {
+    console.log("function: showLogin")
+    byId("login-form").style.display = "block";
+    byId("register-form").style.display = "none";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    byId("show-register").onclick = function(){showRegister()};
+    byId("show-login").onclick = function(){showLogin()};
+})
