7caacc03fa8a539e238351e93ea60e0328581579
jm1412 <112815299+jm1412@users.noreply.github.com>
2023-07-07 18:45:41 +0800

    month generator

--- app_calendar/static/app_calendar/layout.js
+++ app_calendar/static/app_calendar/layout.js
@@ -5,7 +5,44 @@ const byId = document.getElementById.bind(document);
 const byClass = document.getElementsByClassName.bind(document);
 const byTag = document.getElementsByTagName.bind(document);
 
+// Global variables
 var currentYear = "";
+var currentMonth = "";
+    // these will have to be added inside functions soon
+
+const months = {
+    1:"31",2:"29",3:"31",4:"30",5:"31",6:"30",7:"31",8:"31",9:"30",10:"31",11:"30",12:"31"
+};
+
+const monthNames = {
+    1:"january",
+    2:"february",
+    3:"march",
+    4:"april",
+    5:"may",
+    6:"june",
+    7:"july",
+    8:"august",
+    9:"september",
+    10:"october",
+    11:"november",
+    12:"december"
+};
+
+const monthColors = {
+    1: "#F8D7DA",    // Pale Rose
+    2: "#FCE4EC",    // Lavender Blush
+    3: "#F1EEF6",    // Lilac
+    4: "#DDEBF7",    // Baby Blue
+    5: "#E9F7EF",    // Mint Cream
+    6: "#F8F8E7",    // Vanilla
+    7: "#FCE8D5",    // Peachy Beige
+    8: "#F3F3F3",    // Pale Gray
+    9: "#F7ECE1",    // Ivory
+    10: "#F3F8F2",   // Pale Green
+    11: "#EAE7F2",   // Lilac Gray
+    12: "#F2E9E4"    // Soft Peach
+};
 
 function getCookie(name) {
     // For csrf_token
@@ -23,7 +60,7 @@ function getCookie(name) {
         }
     }
     return cookieValue;
-}
+};
 
 function year_onclickday() {
     // When year view day entry is clicked, this function gets called
@@ -82,51 +119,6 @@ function year_onclickday() {
 
 
 function generateYearPlaceholder(currentYear){
-    const months = {
-        1:"31",
-        2:"29",
-        3:"31",
-        4:"30",
-        5:"31",
-        6:"30",
-        7:"31",
-        8:"31",
-        9:"30",
-        10:"31",
-        11:"30",
-        12:"31"
-    };
-
-    const monthNames = {
-        1:"january",
-        2:"february",
-        3:"march",
-        4:"april",
-        5:"may",
-        6:"june",
-        7:"july",
-        8:"august",
-        9:"september",
-        10:"october",
-        11:"november",
-        12:"december"
-    }
-
-    const monthColors = {
-        1: "#F8D7DA",    // Pale Rose
-        2: "#FCE4EC",    // Lavender Blush
-        3: "#F1EEF6",    // Lilac
-        4: "#DDEBF7",    // Baby Blue
-        5: "#E9F7EF",    // Mint Cream
-        6: "#F8F8E7",    // Vanilla
-        7: "#FCE8D5",    // Peachy Beige
-        8: "#F3F3F3",    // Pale Gray
-        9: "#F7ECE1",    // Ivory
-        10: "#F3F8F2",   // Pale Green
-        11: "#EAE7F2",   // Lilac Gray
-        12: "#F2E9E4"    // Soft Peach
-      }
-
     // Generate calendar placeholders
     for (var key in months){
 
@@ -197,7 +189,7 @@ function highlight_today(){
     }
 }
 
-function yearChanger() {
+function yearChanger_y() {
     // When year changer button is clicked,
     // update new year
     // query new year
@@ -217,6 +209,67 @@ function yearChanger() {
     })
 }
 
+function yearChanger_m() {
+    byId("prev-year").addEventListener("click", function(){
+        byId("current-year").innerHTML = Number(byId("current-year").innerHTML)-1
+        currentYear = byId("current-year").innerHTML
+        getCalendarMonth(currentYear)
+
+    })
+
+    byId("next-year").addEventListener("click", function(){
+        byId("current-year").innerHTML = Number(byId("current-year").innerHTML)+1
+        currentYear = byId("current-year").innerHTML
+        getCalendarMonth(currentYear)
+
+    })
+}
+
+function monthCnager_m() {
+    // Similar to year changer but changes month instead
+}
+
+function getCalendarMonth() {
+    // Generate monthly todo
+
+}
+
+function generateMonthPlaceholder() {
+    //  Generate calendar placeholders for month view
+    // currentYear -> int
+    // currentMonth -> int
+
+    console.log("generateMonthPlaceholder");
+
+    // Generate placeholder divs
+    var monthContainer = byId("calendar-month-container");
+    monthContainer.innerHTML += `
+        <div class="row">
+            <div class="col calendar-month" id="mv-month-name"></div>
+        </div>
+        `;
+
+    var targetRow = "";
+
+    for (let i=0; i<5; i++) { // rows
+        monthContainer.innerHTML += `
+            <div class="row calendar-month-days-row" id="month-row-${i}">
+            </div>
+        `;
+        
+        targetRow = byId(`month-row-${i}`);
+        for (let j=1; j<8; j++) { // days per row
+            targetRow.innerHTML+=`
+                <div class="col calendar-month-days" id="day-${j+(7*i)}"></div>
+            `;
+        }
+
+    }
+
+}
+
+
+
 // Main listener/caller
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -231,11 +284,14 @@ document.addEventListener('DOMContentLoaded', function() {
         currentYear = byId("current-year").innerHTML
         generateYearPlaceholder()
         getCalendarYear(currentYear)
-        yearChanger()
+        yearChanger_y()
         // Set today's color
         highlight_today()
     } else if (currentView=="/month"){
-        currentLabel.innerHTML = "Month";
+        // currentLabel.innerHTML = "Month";
+        generateMonthPlaceholder()
+        //yearChanger_m()
+        //monthChanger_m()
     }
 
 })
