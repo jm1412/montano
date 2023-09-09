0000f7b680c0ba1718a12cb71cac9ebc9961a896
jm1412 <112815299+jm1412@users.noreply.github.com>
2023-09-10 04:05:07 +0800

    v3

--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,10 @@
 from django.urls import reverse
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, json
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import *
+import json
 
 # Create your views here.
 def todo_index(request):
