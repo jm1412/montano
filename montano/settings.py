da27a047d6de3f90f6001301d30327185ba8b356
jm1412 <112815299+jm1412@users.noreply.github.com>
2023-10-25 00:48:19 +0800

    static files root

--- montano/settings.py
+++ montano/settings.py
@@ -137,11 +137,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT= [
-    os.path.join(BASE_DIR, 'montano/static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'montano/static'),
 ]
 
 # Default primary key field type
