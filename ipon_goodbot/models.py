aaddbcb5081851d4a4b7801138635384a8bb8773
jm1412 <112815299+jm1412@users.noreply.github.com>
2024-06-29 18:05:43 +0000

    ipon bot models

--- ipon_goodbot/models.py
+++ ipon_goodbot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Gasto(models.Model):
+    telegram_id = models.CharField(max_length=255)
+    amount_spent = models.IntegerField(default=0)
+    date_spent = models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
--- ipon_goodbot/views.py
+++ ipon_goodbot/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def new_entry(request):
+    """Create new entry."""
+    data = json.loads(request.body)
+    telegram_id = data.get("telegram_id", "")
+    amount_spent = data.get("amount_spent", "")
+    date_spent = data.get("date_spent", "")
+
+    gasto = Gasto(
+        telegram_id = telegram_id,
+        amount_spent = amount_spent,
+        date_spent = date_spent
+    )
+    gasto.save()
+
+    return JsonResponse({
+        "message": "success"
+    })
