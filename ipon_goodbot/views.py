bcb8fd2c5a31401974f9e8f724d91aa5dda01034
Juan Montano <jm1412@icloud.com>
2024-06-30 01:25:06 +0100

    import json

--- ipon_goodbot/views.py
+++ ipon_goodbot/views.py
@@ -11,9 +11,14 @@ def gasto_new_entry(request):
     telegram_id = data.get("telegram_id", "")
     amount_spent = data.get("amount", "")
     date_spent = data.get("date", "")
+    
+    datetime_obj = datetime.strptime(date_spent, "%Y-%m-%d")
+    date_obj = datetime_obj.date()
+    
     gasto = Gasto(
         telegram_id = telegram_id,
-        amount_spent = amount_spent
+        amount_spent = amount_spent,
+        date_spent = date_obj
     )
     gasto.save()
 
