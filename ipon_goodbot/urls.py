9e0b559f317f43e163edd0ca022a3a781134273d
Juan Montano <jm1412@icloud.com>
2024-06-30 01:02:41 +0100

    urls

--- ipon_goodbot/urls.py
+++ ipon_goodbot/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from todolist import views
+from ipon_goodbot import views
 
 urlpatterns = [
-    path("", views.todo, name="todo"),
     path("goodbot_postexpense/", views.gasto_new_entry, name="gasto_new_entry"),
 ]
