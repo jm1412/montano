f5cd09d5e4bbd5f48541d0e2f757fe40b4bd3c47
Juan Montano <jm1412@icloud.com>
2024-07-08 13:14:23 +0100

    add user timezones to admin

--- ipon_goodbot/urls.py
+++ ipon_goodbot/urls.py
@@ -3,4 +3,5 @@ from ipon_goodbot import views
 
 urlpatterns = [
     path("goodbot_postexpense/", views.gasto_new_entry, name="gasto_new_entry"),
+    path("get_saved_timezones/", views.get_saved_timezones, name="get_saved_timezones")
 ]
