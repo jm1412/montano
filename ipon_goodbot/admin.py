2ee31f3ebd28f988d615d57add540af25c84a360
Juan Montano <jm1412@icloud.com>
2024-07-11 22:17:41 +0100

    delete models to start fresh

--- ipon_goodbot/admin.py
+++ ipon_goodbot/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Gasto, Timezone
+#from .models import Gasto, Timezone
 
-admin.site.register(Gasto)
-admin.site.register(Timezone)
+#admin.site.register(Gasto)
+#admin.site.register(Timezone)
