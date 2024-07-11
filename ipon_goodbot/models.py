61caf0de900417b13d31efde1e527aae0272c35a
Juan Montano <jm1412@icloud.com>
2024-07-11 22:16:04 +0100

    delete models to start fresh

--- ipon_goodbot/models.py
+++ ipon_goodbot/models.py
@@ -1,26 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class Gasto(models.Model):
-    telegram_id = models.IntegerField()
-    amount_spent = models.FloatField(default=0)
-    date_spent = models.DateTimeField()
-    created_on = models.DateTimeField(auto_now_add=True)
 
-    def serualize(self):
-        return {
-            "telegram_id": self.telegram_id,
-            "amount_spent": self.amount_spent,
-            "date_spent": self.date_spent,
-            "created_on": self.created_on,
-        }
-    
-class Timezone(models.Model):
-    telegram_id = models.CharField(max_length=255)
-    timezone = models.CharField(max_length=255) # Europe/London
-    
-    def serialize(self):
-        return {
-            self.telegram_id: self.timezone,
-        }
         
