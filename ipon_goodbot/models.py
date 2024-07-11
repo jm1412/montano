46bf335c7570661f1f70e5d2fde568183c0aa3a0
Juan Montano <jm1412@icloud.com>
2024-07-11 22:13:13 +0100

    removed tz to try and fix gasto model

--- ipon_goodbot/models.py
+++ ipon_goodbot/models.py
@@ -6,15 +6,13 @@ class Gasto(models.Model):
     amount_spent = models.FloatField(default=0)
     date_spent = models.DateTimeField()
     created_on = models.DateTimeField(auto_now_add=True)
-    user_timezone = models.CharField(max_length=255)
-    
+
     def serualize(self):
         return {
             "telegram_id": self.telegram_id,
             "amount_spent": self.amount_spent,
             "date_spent": self.date_spent,
             "created_on": self.created_on,
-            "user_timezone": self.user_timezone
         }
     
 class Timezone(models.Model):
