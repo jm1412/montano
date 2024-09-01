b536c7e323329646d46de683ac59980035ccfc3c
Juan Montano <jm1412@icloud.com>
2024-09-01 23:09:12 +0100

    token refresh fixed

--- montano/urls.py
+++ montano/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for montano project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# urls.py
 from django.contrib import admin
 from django.urls import path, include
 from montano import views
@@ -23,20 +8,25 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('login/', views.login_view, name="login"),
-    path('login/<str:from_app>', views.login_view, name="login"),
-    path('logout', views.logout_view, name="logout_view"),
-    path('register/', views.register_view, name="register"),
-    path('register/<str:from_app>', views.register_view, name="register"),
+    # Homepage and static pages
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('login/<str:from_app>', views.login_view, name='login_with_redirect'),
+    path('logout/', views.logout_view, name='logout_view'),
+    path('register/', views.register_view, name='register'),
+    path('register/<str:from_app>', views.register_view, name='register_with_redirect'),
     path('admin/', admin.site.urls),
-    path('apps/', views.apps_index, name="apps_index"),
+    
+    # Application-specific URLs
+    path('apps/', views.apps_index, name='apps_index'),
     path('calendar/', include('app_calendar.urls')),
     path('todo/', include('todolist.urls')),
     path('blog/', include('blog.urls')),
     path('skc/', include('skc.urls')),
     path('ipon_goodbot/', include('ipon_goodbot.urls')),
-    path('accounts/', include('accounts.urls')),
+    
+    # Authentication and API URLs
+    path('accounts/', include('accounts.urls')),  # Ensure 'accounts.urls' has the appropriate endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
