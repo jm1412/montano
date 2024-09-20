52d95c44f36e1813c687b8240fb364c392829b31
Juan Montano <jm1412@icloud.com>
2024-09-20 11:46:24 +0100

    use request instead of wget to avoid ssl problems

--- rss/views.py
+++ rss/views.py
@@ -1,9 +1,9 @@
-import subprocess
 import os
 import time
 from django.http import HttpResponse
 from bs4 import BeautifulSoup
 from xml.etree.ElementTree import Element, SubElement, tostring
+import requests
 
 def scrape_mangapark(request, num_pages=2):
     print("running: scrape_mangapark")
@@ -20,14 +20,14 @@ def scrape_mangapark(request, num_pages=2):
     else:
         print("downloading new file")
         try:
-            # Use wget to fetch the webpage
-            with open(output_file, 'w', encoding='utf-8') as file:
-                for page in range(1, num_pages + 1):
-                    url = f"{base_url}?page={page}"
-                    result = subprocess.run(['wget', '-O', file.name, url], capture_output=True, text=True)
-                    if result.returncode != 0:
-                        print(f"Error fetching page: {result.stderr}")
-                        return HttpResponse(f"Error fetching page: {result.stderr}", status=500)
+            for page in range(1, num_pages + 1):
+                url = f"{base_url}?page={page}"
+                response = requests.get(url)
+                if response.status_code == 200:
+                    with open(output_file, 'a', encoding='utf-8') as file:  # Append to file
+                        file.write(response.text)
+                else:
+                    return HttpResponse(f"Error fetching page: {response.status_code}", status=500)
 
             # Read the content from the output file
             with open(output_file, 'r', encoding='utf-8') as file:
@@ -41,15 +41,14 @@ def scrape_mangapark(request, num_pages=2):
     soup = BeautifulSoup(html_content, 'html.parser')
     manga_dict = {}
     print("starting to extract")
-    
+
     # Extract manga titles and chapter links
-    manga_items = soup.select('.pl-3')  # Select the parent container
+    manga_items = soup.select('.pl-3')
 
     for item in manga_items:
         manga_title = item.find('h3').get_text(strip=True)
-        chapters = item.select('a.link-hover.link-primary')  # Select chapter links within the same parent
+        chapters = item.select('a.link-hover.link-primary')
 
-        # Create a list of chapter links for this manga
         chapter_list = [(chapter.get_text(strip=True), chapter['href']) for chapter in chapters]
 
         if chapter_list:
